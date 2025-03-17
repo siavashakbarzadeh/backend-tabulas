475c18f757d60823758931ecf6c39700d77ccd93
mralikeshtkar <alikeshtkar262@gmail.com>
2025-03-17 13:04:12 +0100

    media resource

--- app/Http/Controllers/V1/Media/WebMediaController.php
+++ app/Http/Controllers/V1/Media/WebMediaController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\V1\Media;
 
 use App\Models\Media;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class WebMediaController
 {
+    /**
+     * @param $media
+     * @param $file
+     * @return BinaryFileResponse
+     */
     public function download($media, $file)
     {
         $media = Media::findOrFail($media);
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Facades\Api\ApiResponse as ApiResponseFacade;
 use App\Models\Application;
 use App\Policies\Application\ApplicationPolicy;
 use App\Responses\Api\ApiResponse;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->_registerPolicies();
         $this->_registerFacades();
+        $this->_morphMap();
     }
 
     /**
@@ -44,5 +46,15 @@ class AppServiceProvider extends ServiceProvider
     public function _registerPolicies(): void
     {
         Gate::policy(Application::class, ApplicationPolicy::class);
+    }
+
+    /**
+     * @return void
+     */
+    public function _morphMap(): void
+    {
+        Relation::morphMap([
+            'application' => Application::class,
+        ]);
     }
 }
