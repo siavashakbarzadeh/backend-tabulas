7e9a2697867096dd4445291c345f0b3a296f1e74
mralikeshtkar <alikeshtkar262@gmail.com>
2025-03-17 12:59:20 +0100

    media resource

--- app/Http/Controllers/V1/Media/WebMediaController.php
+++ app/Http/Controllers/V1/Media/WebMediaController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\V1\Media;
 
 use App\Models\Media;
+use Illuminate\Support\Facades\Storage;
 
 class WebMediaController
 {
-    public function download($media,$file)
+    public function download($media, $file)
     {
-        $media = Media::find($media);
-        dd($media,$file);
+        $media = Media::findOrFail($media);
+        $files = $media->files;
+        if (!array_key_exists($file, $files)) {
+            abort(404);
+        }
+        return response()->download(Storage::disk($media->disk)->path($file));
     }
 }
