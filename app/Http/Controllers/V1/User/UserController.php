209118a96645b85eb61ea15ce72038da2f067ad7
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-18 23:04:58 +0100

    Update UserController.php

--- app/Http/Controllers/V1/User/UserController.php
+++ app/Http/Controllers/V1/User/UserController.php
@@ -5,18 +5,32 @@ namespace App\Http\Controllers\V1\User;
 use App\Facades\Api\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\User\SingleUserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function user(Request $request){
-        return ApiResponse::addData('user', new SingleUserResource($request->user()))
-            ->success(trans('messages.success'));
+    public function user(Request $request)
+    {
+        return ApiResponse::addData(
+            'user',
+            new SingleUserResource($request->user())
+        )->success(trans('messages.success'));
     }
+
     public function search(Request $request)
-{
-    $query = $request->get('query', '');
-    $users = User::where('name', 'like', "%{$query}%")->limit(10)->get();
-    return ApiResponse::addData('users', $users)->success();
-}
+    {
+        $query = $request->get('query', '');
+
+        // Query the User model, not the Resource
+        $users = User::where('name', 'like', "%{$query}%")
+            ->limit(10)
+            ->get();
+
+        // Wrap the collection in your Resource class
+        return ApiResponse::addData(
+            'users',
+            SingleUserResource::collection($users)
+        )->success();
+    }
 }
